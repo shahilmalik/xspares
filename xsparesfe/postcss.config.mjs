ccb5c1a0221861beed23871d20fa02f5862bd202
Ebrahim <ebrahim@victopialabs.com>
2024-12-13 01:26:49 +0530

    implemented topnavbar

--- xsparesfe/postcss.config.mjs
+++ xsparesfe/postcss.config.mjs
@@ -1,19 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/page_components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/common/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
   },
-  plugins: [],
-}
+};
+
+export default config;
