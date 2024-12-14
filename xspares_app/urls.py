19d6776b8576b25598551b225b05529e4110f0c6
Shahil Malik <shahil@victopialabs.com>
2024-12-15 03:28:20 +0530

    Created APIs for registration of users, vendors, and staff with email verfication using otp

--- xspares_app/urls.py
+++ xspares_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import get_items_by_car
+from .views import get_items_by_car, register_user, send_otp, verify_otp, register_vendor
 urlpatterns = [
-    path('get-items-by-car/',get_items_by_car,name="get-items-by-car")
+    path('register-user/',register_user,name="register-user"),
+    path('register-vendor/',register_vendor,name="register-vendor"),
+    path('send-otp/',send_otp,name="send-otp"),
+    path('verify-otp/',verify_otp,name="verify-otp"),
+    path('get-items-by-car/',get_items_by_car,name="get-items-by-car"),
 ]
 
